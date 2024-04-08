76d92de6c5c2d34044f0fb98c472bba950368dee
WANG Rui <wangrui@loongson.cn>
2024-04-08 14:47:01 +0800

    Benchmarks: Micro: char-methods-bench_non_ascii_char_to_lowercase

---
+++ benchmarks/micro/char-methods-bench_non_ascii_char_to_lowercase/src/main.rs
@@ -0,0 +1,11 @@
+use std::hint::black_box;
+
+fn main() {
+    for _ in 1..=5000 {
+        (128..=255)
+            .cycle()
+            .take(10_000)
+            .map(|b| black_box(char::from(b)).to_lowercase())
+            .count();
+    }
+}
