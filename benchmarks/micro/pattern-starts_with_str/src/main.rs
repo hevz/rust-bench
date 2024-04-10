5d2b1dd5b56ff804b03fd200ba4b5347faf44dcf
WANG Rui <wangrui@loongson.cn>
2024-04-10 15:53:05 +0800

    Benchmarks: Micro: pattern-starts_with_str

---
+++ benchmarks/micro/pattern-starts_with_str/src/main.rs
@@ -0,0 +1,10 @@
+use std::hint::black_box;
+
+fn main() {
+    let text = black_box("kdjsfhlakfhlsghlkvcnljknfqiunvcijqenwodind");
+    for _ in 1..=2000000 {
+        for _ in 0..1024 {
+            black_box(text.starts_with("k"));
+        }
+    }
+}
