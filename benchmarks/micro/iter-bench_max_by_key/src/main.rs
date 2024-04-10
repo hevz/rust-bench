451784fbca09554e5334c4caca5952fe1b338b65
WANG Rui <wangrui@loongson.cn>
2024-04-10 17:18:19 +0800

    Benchmarks: Micro: iter-bench_max_by_key

---
+++ benchmarks/micro/iter-bench_max_by_key/src/main.rs
@@ -0,0 +1,12 @@
+use std::hint::black_box;
+
+fn scatter(x: i32) -> i32 {
+    (x * 31) % 127
+}
+
+fn main() {
+    for _ in 1..=5000000 {
+        let it = 0..100;
+        black_box(it.map(black_box).max_by_key(|&x| scatter(x)));
+    }
+}
